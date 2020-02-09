2f38a8e189225ab0afbc647d476e2061e605133c
Ayrton Casella <awcasella461@gmail.com>
2020-02-09 19:18:03 -0300

    Matlab fun

---
+++ MatLab fun stuff/fun.m
@@ -0,0 +1,20 @@
+
+% This is a Graphical User Interface for playing and solving the 15-Puzzle game.
+fifteen
+
+% mineswiper
+xpbombs
+
+% plays hallelujah chorus
+load handel
+sound(y,Fs)
+
+% Asteroids on Simulink
+eml_asteroids
+
+
+% Tic-Tac-Toe 
+sf_tictacflow
+
+% GUI to see distances between places around the world
+wrldtrv
